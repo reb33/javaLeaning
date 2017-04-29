26dd4c3551809f33e4c5786b0bd13774d956bbaa
reb33 <konstbar@gmail.com>
2017-04-29 21:41:29 +0300

    certification

---
+++ mavenProject1/src/main/java/certification/champter2/Sample1Modules.java
@@ -0,0 +1,16 @@
+package certification.champter2;
+
+/**
+ * Created by Konstantin on 02.04.2017.
+ */
+public class Sample1Modules {
+
+    public static void main(String[] args) {
+        double d1 = 65.01 % 7;
+        int i1 = -65 % 7;
+        int i2 = 65 %  -7;
+        System.out.println(d1);
+        System.out.println(i1);
+        System.out.println(i2);
+    }
+}
