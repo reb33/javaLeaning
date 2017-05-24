f30fbefce6a89cb39eeb4d0b08ed55a2ef0121ef
konstantin <konstbar@gmail.com>
2017-05-24 15:35:43 +0300

    certification

--- mavenProject1/src/main/java/certification/champter4/protectedCheck/sub/UglyDuck.java
+++ mavenProject1/src/main/java/certification/champter4/protectedCheck/sub/UglyDuck.java
@@ -4,4 +4,13 @@ package certification.champter4.protectedCheck.sub;
  * Created by konstantin on 24.05.2017.
  */
 public class UglyDuck extends Swan {
+
+    void check7(){
+        UglyDuck uglyDuck = new UglyDuck();
+        uglyDuck.getNameBird();
+    }
+    void check8(){
+        Swan swan = new Swan();
+//        swan.getNameBird(); //доступен только текущий класс или наследники, родительские НЕТ!!!!
+    }
 }
