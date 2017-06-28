b22a393d64725f0f16d38a08d63b8442c892c30a
konstantin <konstbar@gmail.com>
2017-06-28 15:16:52 +0300

    Merge branch 'master' of https://github.com/reb33/javaLeaning
    # Conflicts:
    #	mavenProject1/src/main/java/certification/champter2/Sample7Circles.java

---
+++ mavenProject1/src/main/java/certification/champter3/Sample3String2.java
@@ -0,0 +1,13 @@
+package certification.champter3;
+
+/**
+ * Created by konstantin on 28.06.2017.
+ */
+public class Sample3String2 {
+
+    public static void main(String[] args) {
+        String str = " ";
+        str = str.trim();
+        System.out.println(str.equals("") +" "+str.isEmpty());
+    }
+}
