b6b5dc570faea3ee06c34f0d2a09d244f86e3865
user <konstbar@gmail.com>
2018-01-26 10:50:38 +0300

    sert

---
+++ mavenProject1/src/main/java/certification2/tricks/NullAndPrim.java
@@ -0,0 +1,10 @@
+package certification2.tricks;
+
+public class NullAndPrim {
+
+    public static void main(String[] args) {
+        int i=1;
+        //приметивы нельзя сравнивать с null - ош. комп.
+//        System.out.println(i==null);
+    }
+}
