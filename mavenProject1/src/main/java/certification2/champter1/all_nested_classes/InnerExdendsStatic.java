2853a3cf10312988ff0afa3c0bff195aa7c02f7e
konstbar <konstbar@gmail.com>
2018-02-19 19:37:14 +0300

    cert

---
+++ mavenProject1/src/main/java/certification2/champter1/all_nested_classes/InnerExdendsStatic.java
@@ -0,0 +1,15 @@
+package certification2.champter1.all_nested_classes;
+
+public class InnerExdendsStatic {
+
+    class A{}
+
+    //static не может быть наследован от не static
+//    static class B extends A{}
+
+    static class C{}
+
+    class D extends C{}
+
+//    static class E extends D{}
+}
