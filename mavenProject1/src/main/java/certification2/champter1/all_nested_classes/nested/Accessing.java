d0d3feab7ac742b8a6e5a3470480d24f7227e912
user <konstbar@gmail.com>
2018-02-22 14:58:52 +0300

    sert

--- mavenProject1/src/main/java/certification2/champter1/all_nested_classes/nested/Accessing.java
+++ mavenProject1/src/main/java/certification2/champter1/all_nested_classes/nested/Accessing.java
@@ -1,6 +1,6 @@
 package certification2.champter1.all_nested_classes.nested;
 
-import static certification2.champter1.all_nested_classes.nested.Accessing.Nested.*;
+import static certification2.champter1.all_nested_classes.nested.Accessing.Nested.i3;
 
 /**
  * Created by konstantin on 18.08.2017.
@@ -18,6 +18,7 @@ public class Accessing {
         private static void m2(){}
         static void m3(){}
         void m4(){
+            //члены внешнего класса доступны в nested
             new Accessing().m1();
             System.out.println(new Accessing().oi1);
             om1();
@@ -25,6 +26,12 @@ public class Accessing {
     }
 
     public static void main(String[] args) {
+        //для доступа к эл-ам nested класса ну но обращаться через объект или через имя класса(если статичны)
+        //можно указать статик импорт для доступа к статичным элеметам (i3 доступно, m3 не доступно)
+        //можно указать статик импорт для всего класса через Nested.*
+        // при этом будут импортированы все элементы кроме private
+        // в том же классе можно указать статик импорт для приватных элементов,
+        // но только отдельный импорт для каждого
         Nested nested = new Nested();
         System.out.println(nested.i1);
         System.out.println(Nested.i2);
@@ -34,7 +41,7 @@ public class Accessing {
         i3++;
         Nested.m2();
 //        m2();
-        m3();
+//        m3();
     }
 }
 
